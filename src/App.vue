4831773bb39036e9550f06306b6da0979ea45755
SamuelNdreu <samwellsoficcial@gmail.com>
2023-01-09 16:20:15 +0100

    create project

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+
+</script>
+
+<template>
+  <div>
+    <h3>Ciao</h3>
+  </div>
+
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables'
+</style>
